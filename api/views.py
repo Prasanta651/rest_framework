a36cad0b370f9b7c4e2312145a66b6e59325ea38
prasanta <prasantabadatya651@gmail.com>
2025-02-03 11:26:09 +0530

    Add basic flow for using serializer

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+from api.models import Product
+from api.serializers import ProductSerializer
+
+
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return JsonResponse({
+        'data': serializer.data
+    })
+
--- restframework/urls.py
+++ restframework/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products/', views.product_list),
 ]
