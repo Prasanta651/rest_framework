17bd533230a94d25ba9f4ee0f96b6f214e40f64e
prasanta <prasantabadatya651@gmail.com>
2025-02-06 11:10:13 +0530

    Return response through Response class for consistency and proper API response handling

--- api/views.py
+++ api/views.py
@@ -2,17 +2,16 @@ from django.db.models import Max
 from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from api.models import Product
 from api.serializers import ProductSerializer, ProductInfoSerializer
 
-
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return JsonResponse({
-        'data': serializer.data
-    })
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
