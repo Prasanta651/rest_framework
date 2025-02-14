ce0108cb8ef5e13f875cd1e05e223b15fbc8026c
prasanta <prasantabadatya651@gmail.com>
2025-02-14 11:27:35 +0530

    Link OrderItem model to Order admin using inline (TabularInline)

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from api.models import Order
-# Register your models here.
+from api.models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
 class OrderAdmin(admin.ModelAdmin):
+    inlines = [ OrderItemInline ]
     raw_id_fields = ("user",)
     list_display = [field.attname for field in Order._meta.fields]
 
